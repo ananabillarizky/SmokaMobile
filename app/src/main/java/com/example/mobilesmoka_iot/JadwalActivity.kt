3e23c2cdb2fc9ebc7a3c2c868770d328fe4ae342
nuryusufa <riyagung123@gmail.com>
2023-06-17 15:49:20 +0700

    update jadwal pakan

--- app/src/main/java/com/example/mobilesmoka_iot/JadwalActivity.kt
+++ app/src/main/java/com/example/mobilesmoka_iot/JadwalActivity.kt
@@ -5,29 +5,57 @@ import android.app.TimePickerDialog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageView
-import android.widget.Spinner
-import android.widget.TextView
+import android.util.Log
+import android.widget.*
+import com.google.firebase.firestore.FirebaseFirestore
 import java.util.Calendar
 
 class JadwalActivity : AppCompatActivity() {
     private lateinit var tvtime1 : TextView
     private lateinit var editButton : Button
+    private lateinit var saveButton : Button
 
     private lateinit var tvtime2 : TextView
     private lateinit var editButton2 : Button
+    private lateinit var saveButton2 : Button
 
     private lateinit var tvtime3 : TextView
     private lateinit var editButton3 : Button
-
+    private lateinit var saveButton3 : Button
+    private lateinit var firestore: FirebaseFirestore
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_jadwal)
 
+        firestore = FirebaseFirestore.getInstance()
+        // Mendapatkan referensi dokumen dari Firestore
+        val jadwalPakanRef = firestore.collection("jadwal_pakan")
+
         tvtime1  = findViewById(R.id.tvtime1)
         editButton = findViewById(R.id.editButton )
+        saveButton = findViewById(R.id.saveButton)
+
+
+
+        // Mendapatkan data dari Firestore
+        jadwalPakanRef.document("jadwal_pakan1").get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    // Dokumen ada, ambil nilai properti "time" dari dokumen
+                    val timeValue = document.getString("time")
+
+                    // Atur nilai timeValue ke tvTime
+                    tvtime1.text = timeValue
+                } else {
+                    // Dokumen tidak ditemukan, atur nilai default "00:00" ke tvTime
+                    tvtime1.text = "00:00"
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Gagal mengambil data dari Firestore, tampilkan pesan kesalahan
+                Toast.makeText(this, "Gagal mengambil data dari Firestore: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
 
 
         editButton.setOnClickListener{
@@ -40,8 +68,46 @@ class JadwalActivity : AppCompatActivity() {
             }, startHour,startMinute, true).show()
         }
 
+        saveButton.setOnClickListener {
+            val timeValue = tvtime1.text.toString()
+
+        // Simpan nilai tvtime ke Firestore
+            val jadwalPakanRef = firestore.collection("jadwal_pakan").document("jadwal_pakan1")
+            val data = hashMapOf("time" to timeValue)
+            Log.d("Cek",data.toString());
+
+            jadwalPakanRef.set(data)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Nilai berhasil disimpan di Firestore", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Gagal menyimpan nilai di Firestore", Toast.LENGTH_SHORT).show()
+                }
+
+        }
+
         tvtime2  = findViewById(R.id.tvtime2)
         editButton2 = findViewById(R.id.editButton2 )
+        saveButton2 = findViewById(R.id.saveButton2)
+
+        // Mendapatkan data dari Firestore
+        jadwalPakanRef.document("jadwal_pakan2").get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    // Dokumen ada, ambil nilai properti "time" dari dokumen
+                    val timeValue = document.getString("time")
+
+                    // Atur nilai timeValue ke tvTime
+                    tvtime2.text = timeValue
+                } else {
+                    // Dokumen tidak ditemukan, atur nilai default "00:00" ke tvTime
+                    tvtime2.text = "00:00"
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Gagal mengambil data dari Firestore, tampilkan pesan kesalahan
+                Toast.makeText(this, "Gagal mengambil data dari Firestore: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
 
         editButton2.setOnClickListener{
             val currentTime = Calendar.getInstance()
@@ -53,8 +119,48 @@ class JadwalActivity : AppCompatActivity() {
             }, startHour,startMinute, true).show()
         }
 
+        saveButton2.setOnClickListener {
+            val timeValue = tvtime2.text.toString()
+
+            // Simpan nilai tvtime ke Firestore
+            val jadwalPakanRef = firestore.collection("jadwal_pakan").document("jadwal_pakan2")
+            val data = hashMapOf("time" to timeValue)
+            Log.d("Cek",data.toString());
+
+            jadwalPakanRef.set(data)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Nilai berhasil disimpan di Firestore", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Gagal menyimpan nilai di Firestore", Toast.LENGTH_SHORT).show()
+                }
+
+        }
+
         tvtime3  = findViewById(R.id.tvtime3)
         editButton3 = findViewById(R.id.editButton3 )
+        saveButton3 = findViewById(R.id.saveButton3)
+
+        // Mendapatkan data dari Firestore
+        jadwalPakanRef.document("jadwal_pakan3").get()
+            .addOnSuccessListener { document ->
+                if (document.exists()) {
+                    // Dokumen ada, ambil nilai properti "time" dari dokumen
+                    val timeValue = document.getString("time")
+
+                    // Atur nilai timeValue ke tvTime
+                    tvtime3.text = timeValue
+                } else {
+                    // Dokumen tidak ditemukan, atur nilai default "00:00" ke tvTime
+                    tvtime3.text = "00:00"
+                }
+            }
+            .addOnFailureListener { exception ->
+                // Gagal mengambil data dari Firestore, tampilkan pesan kesalahan
+                Toast.makeText(this, "Gagal mengambil data dari Firestore: ${exception.message}", Toast.LENGTH_SHORT).show()
+            }
+
+
         editButton3.setOnClickListener{
             val currentTime = Calendar.getInstance()
             val startHour = currentTime.get(Calendar.HOUR_OF_DAY)
@@ -63,6 +169,24 @@ class JadwalActivity : AppCompatActivity() {
             TimePickerDialog(this,TimePickerDialog.OnTimeSetListener { view, hourofDay, minute ->
                 tvtime3 .setText("$hourofDay : $minute")
             }, startHour,startMinute, true).show()
+        }
+
+        saveButton3.setOnClickListener {
+            val timeValue = tvtime3.text.toString()
+
+            // Simpan nilai tvtime ke Firestore
+            val jadwalPakanRef = firestore.collection("jadwal_pakan").document("jadwal_pakan3")
+            val data = hashMapOf("time" to timeValue)
+            Log.d("Cek",data.toString());
+
+            jadwalPakanRef.set(data)
+                .addOnSuccessListener {
+                    Toast.makeText(this, "Nilai berhasil disimpan di Firestore", Toast.LENGTH_SHORT).show()
+                }
+                .addOnFailureListener {
+                    Toast.makeText(this, "Gagal menyimpan nilai di Firestore", Toast.LENGTH_SHORT).show()
+                }
+
         }
 
         val img_back = findViewById<ImageView>(R.id.img_back)
